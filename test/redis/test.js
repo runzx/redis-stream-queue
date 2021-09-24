0f71d6ef9fb41495e87297f62cd0d57010e763a1
zhaixiang <843476168@qq.com>
2021-09-24 17:25:16 +0800

    feat: xadd sendMsg

--- test/redis/test.js
+++ test/redis/test.js
@@ -6,9 +6,13 @@ const { objToArr, arr2obj, arr2item } = require("../../lib/util")
 let res
 let redis = new RedisBase({ password: 'zx2962', sKey: 'abc', gKey: 'bosstg', cKey: 'zx' })
 
-redis.getMsgById('16323987503954').then(res => {
-  console.log('msg:', res)
+redis.sendMsg({ i: '16323987503954', data: 158 }).then(res => {
+  console.log('msgId:', res)
 })
+
+// redis.getMsgById('16323987503954').then(res => {
+//   console.log('msg:', res)
+// })
 
 // redis.getMsgByConsumer('mm').then(res => {
 //   console.log('res:', res)
